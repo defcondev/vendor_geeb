1e5a2012bd8f0028bf523d36eba9e52c08a4c1e0
g3t <batline@live.com>
2013-03-03 15:40:28 -0500

    fresh

---
+++ config/hn_geeb.mk
@@ -0,0 +1,23 @@
+# Copyright (C) 2012 AOSP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include houstonn common configuration
+include vendor/houstonn/config/hn_common.mk
+
+# Override AOSP build properties
+#PRODUCT_NAME := geeb
+#PRODUCT_BRAND := LG
+#PRODUCT_MODEL := LG-E973
+#PRODUCT_MANUFACTURER := LGE
+#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occam/geeb:4.2/JOP40C/527662:user/release-keys" PRIVATE_BUILD_DESC="occam-user 4.2 JOP40C 527662 release-keys"
