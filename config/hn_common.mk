37a13ce327bb26236a45c4048633d395d90bf3be
houstonn <houstonn@gmail.com>
2013-03-15 18:20:21 -0400

    Revert "Revert "remove benchmark for now""
    This reverts commit 9ca76fa1f0941ee4ba74b3d9a5f35fadc4f0161e.

--- config/hn_common.mk
+++ config/hn_common.mk
@@ -29,8 +29,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
     vendor/houstonn/prebuilt/common/apk/CMFileManager.apk:system/app/CMFileManager.apk \
-    vendor/houstonn/prebuilt/common/apk/com.miui.player-1.apk:system/app/com.miui.player-1.apk \
-	vendor/houstonn/prebuilt/common/apk/BBQBenchmark.apk:system/app/BBQBenchmark.apk
+    vendor/houstonn/prebuilt/common/apk/com.miui.player-1.apk:system/app/com.miui.player-1.apk
+#	vendor/houstonn/prebuilt/common/apk/BBQBenchmark.apk:system/app/BBQBenchmark.apk
 	
 # bootani I chose
 #PRODUCT_COPY_FILES += \
